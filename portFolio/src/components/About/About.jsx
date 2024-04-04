b709ca190197372b9d10eadd7ded5097a5973194
Shrawan <shrawanre07@gmail.com>
2024-04-04 14:02:21 +0530

    commit

---
+++ portFolio/src/components/About/About.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/project.png")}
+          alt="Me sitting with a laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Fullstack Developer</h3>
+              <p>
+                I'm a fullstack developer with experience in building responsive
+                and optimized sites
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>React Native Developer</h3>
+              <p>
+                I have experience in developing native apps for Android and IOS
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
+            <div className={styles.aboutItemText}>
+              <h3>WEB3</h3>
+              <p>
+                I'm a Blockchain developer,can make Etherium based applications using Solidity
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
---
+++ portFolio/src/components/Hero/Hero.jsx
@@ -0,0 +1,28 @@
+
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Shrawan</h1>
+        <p className={styles.description}>
+          A full-stack developer and WEB3 developer. Reach out if you'd like to connect with me!
+        </p>
+        <a href="https://www.linkedin.com/in/shrawan-wandhekar-1b1894273/" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/shrawan.jpg")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
